15ccce232b7406080e159ce73dec1feace41c375
jun2k5 <jun2k5@naver.com>
2026-04-10 18:08:19 +0900

    🐛 fix      : prod 세팅 URL 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -5,3 +5,10 @@ from config.settings.base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ["oz-pgti.duckdns.org", "13.211.150.226", "localhost", "127.0.0.1"]
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
