700e3e00dd6eb1d4f088cf42d10171cb1310c04b
jun2k5 <jun2k5@naver.com>
2026-04-10 17:46:21 +0900

    ✨ feat     : nginx 443 포트 추가 및 도메인 추가

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,4 +4,4 @@ from config.settings.base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["13.211.150.226", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["oz-pgti.duckdns.org", "13.211.150.226", "localhost", "127.0.0.1"]
