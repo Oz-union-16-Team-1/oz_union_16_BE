995de85aa80b6b0ca1542cff0199e4ce33ec90e0
jun2k5 <jun2k5@naver.com>
2026-04-10 19:06:45 +0900

    🐛 fix      : static root 명시

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -6,9 +6,9 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["oz-pgti.duckdns.org", "13.211.150.226", "localhost", "127.0.0.1"]
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
