4e82cfca58245948ed371f7a7d9edddcd4748b31
pcw8233 <pcw3511@gmail.com>
2026-04-14 18:29:43 +0900

    game_models.py 수정 & serializers.py 작성

--- apps/games/models.py
+++ apps/games/models.py
@@ -1,7 +1,30 @@
 from django.db import models
 from django.db.models import Q
+from django.conf import settings
 
+# 1. 유저별 좋아요 기록
+class UserGamelike(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="game_likes",
+    )
+    game_id = models.IntegerField() # IGDB 게임_ID
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        db_table = "user_game_likes"
+        constraints = [
+            models.UniqueConstraint(
+                fields=[
+                    'user',
+                    'game_id'
+                ],
+                name="user_game_likes_unique"
+            ),
+        ]
+
+# 2. 게임당 총 좋아요 수
 class GameLikeCount(models.Model):
     game_like_id = models.BigAutoField(primary_key=True)
     game_id = models.IntegerField(unique=True)  # 집계 테이블: 게임당 1행만 유지
@@ -20,13 +43,13 @@ class GameLikeCount(models.Model):
             ),
         ]
 
-
+#3. 블랙리스트 관리
 class GameExclusion(models.Model):
     game_exclusion_id = models.BigAutoField(primary_key=True)
     game_id = models.IntegerField(
         unique=True
-    )  # 블랙리스트는 게임당 1건만 허용 (사유는 reason 문자열로 통합 관리)
-    title = models.CharField(max_length=30)
+    )
+    title = models.CharField(max_length=255)
     reason = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
