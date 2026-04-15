d1690a6808f074a8b9b337c4055eb8bfef23167a
jun2k5 <jun2k5@naver.com>
2026-04-15 16:45:55 +0900

    ✨ feat     : 409 에러 커스텀 클래스 추가

--- apps/core/exceptions.py
+++ apps/core/exceptions.py
@@ -1,4 +1,11 @@
+from rest_framework import status
+from rest_framework.exceptions import APIException
 from rest_framework.views import exception_handler
+
+
+class ConflictException(APIException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = "이미 존재하는 데이터입니다."
 
 
 def custom_exception_handler(exc, context):
