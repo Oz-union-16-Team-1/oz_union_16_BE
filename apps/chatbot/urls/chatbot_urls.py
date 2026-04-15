c8a5e2c8f93c6dfd3fa782803fc8c1c08a19e391
dodzn89-cell <dodzn89@naver.com>
2026-04-15 18:19:31 +0900

    💡 chore    : 기능 추가 없이 코드 수정 (오타, 주석 등)

--- apps/chatbot/urls/chatbot_urls.py
+++ apps/chatbot/urls/chatbot_urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ChatbotMessageAPIView, ChatbotStreamAPIView
+from apps.chatbot.views.chatbot_views import ChatbotMessageAPIView, ChatbotStreamAPIView
 
 urlpatterns = [
     path("messages", ChatbotMessageAPIView.as_view(), name="chatbot-messages"),
