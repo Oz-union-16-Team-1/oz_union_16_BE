d8ecd16f680968b1d56db49f02569b6203f2b376
wooryun <rhrjs4@naver.com>
2026-04-15 18:20:36 +0900

    💡 chore    : Core models에 uuid 내용 추가 및 Survey models 수정

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,9 +1,19 @@
+import uuid
+
 from django.db import models
 
 
 class TimeStampedModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성일")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="수정일")
+
+    class Meta:
+        abstract = True
+
+
+# UUID를 기본 PK로 사용하는 공통 모델 추가
+class UUIDModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     class Meta:
         abstract = True
