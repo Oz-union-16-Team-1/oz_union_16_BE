ec047311213fb8de3a0c3f9728af30da23228c07
jun2k5 <jun2k5@naver.com>
2026-04-27 13:36:14 +0900

    🐛 fix      : test_check_pwd 들여쓰기 오류 해결

--- apps/users/tests/test_check_pwd.py
+++ apps/users/tests/test_check_pwd.py
@@ -32,17 +32,17 @@ class MyInfoTest(TestCase):
         self.client = APIClient()
 
 
-def test_password_check_success(self):
-    """비밀번호 확인 API 성공 테스트"""
-    self.client.force_authenticate(user=self.user)
-    response = self.client.post(
-        reverse("password-check"), data={"password": self.password}
-    )
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_password_check_success(self):
+        """비밀번호 확인 API 성공 테스트"""
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            reverse("check-password"), data={"password": self.password}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
-def test_password_check_fail(self):
-    """비밀번호 확인 API 실패 테스트"""
-    self.client.force_authenticate(user=self.user)
-    response = self.client.post(reverse("password-check"), data={"password": "wrong"})
-    self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_password_check_fail(self):
+        """비밀번호 확인 API 실패 테스트"""
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(reverse("check-password"), data={"password": "wrong"})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
