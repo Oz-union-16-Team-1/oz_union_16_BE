5074242682fb84732afe3cfb1250a1be65cce6b2
pcw8233 <pcw3511@gmail.com>
2026-04-28 14:03:03 +0900

    ✨ feat     : apps/core/igdb.py, game_name mapping 수정

--- apps/games/serializer/game_list_top100_serializers.py
+++ apps/games/serializer/game_list_top100_serializers.py
@@ -1,6 +1,6 @@
-# apps/games/serializer/game_list_top100_serializers.py
 from rest_framework import serializers
 
+from apps.core.igdb import IGDB
 from apps.games.models import Game
 
 
@@ -10,33 +10,6 @@ class GameTop100Serializer(serializers.ModelSerializer):
     total_rating = serializers.FloatField(read_only=True)
     total_rating_count = serializers.IntegerField(read_only=True)
     first_release_date = serializers.DateTimeField(format="%Y-%m-%d", read_only=True)
-
-    GENRE_NAME_MAP = {
-        1: "액션",
-        2: "포인트 앤 클릭",
-        4: "격투",
-        5: "슈팅",
-        7: "음악",
-        8: "플랫폼",
-        9: "퍼즐",
-        10: "레이싱",
-        11: "실시간 전략(RTS)",
-        12: "역할수행(RPG)",
-        13: "시뮬레이션",
-        14: "스포츠",
-        15: "전략",
-        16: "턴제 전략(TBS)",
-        24: "전술",
-        25: "핵 앤 슬래시",
-        26: "퀴즈/상식",
-        30: "핀볼",
-        31: "어드벤처",
-        32: "인디",
-        33: "아케이드",
-        34: "비주얼 노벨",
-        35: "카드 및 보드 게임",
-        36: "모바(MOBA)",
-    }
 
     class Meta:
         model = Game
@@ -62,11 +35,8 @@ class GameTop100Serializer(serializers.ModelSerializer):
 
         if ret.get("genres"):
             ret["genres"] = [
-                self.GENRE_NAME_MAP[gid]
-                for gid in ret["genres"]
-                if gid in self.GENRE_NAME_MAP
+                IGDB.GENRE_NAME_MAP.get(gid, f"기타({gid})") for gid in ret["genres"]
             ]
-
         cover_id = ret.get("cover")
         if cover_id:
             cover_id_str = str(cover_id)
