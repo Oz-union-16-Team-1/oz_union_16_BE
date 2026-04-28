f3461f0234b43adb1d5e3261b753c19b98365d24
pcw8233 <pcw3511@gmail.com>
2026-04-28 10:28:17 +0900

    🐛 fix      : 오타 수정

--- apps/games/urls.py
+++ apps/games/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.games.view.game_list_top100_views import GameTop100View
 
 urlpatterns = [
-    path("lst/top100", GameTop100View.as_view(), name="game_list_top100"),
+    path("list/top100", GameTop100View.as_view(), name="game_list_top100"),
 ]
