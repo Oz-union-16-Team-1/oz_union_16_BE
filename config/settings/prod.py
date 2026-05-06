898dc5d4349cbe08f1cdf056e93034a3f84723d2
Justman-yzz <a01088744395@gmail.com>
2026-05-06 13:24:45 +0900

    🐛 fix: prod staticfiles 설정 충돌(staticfiles.E002) 해결

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -16,6 +16,7 @@ ALLOWED_HOSTS = [
 
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR.parent, "static")
+STATICFILES_DIRS = []
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR.parent, "media")
